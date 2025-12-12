7855fc67f89d4540326d928e0ae4815371df45e7
onkarkorpe <ravikorpe2002@gmail.com>
2025-12-12 18:09:25 +0530

    Implemented Navigation graph and routes

---
+++ app/src/main/java/com/kalcreations/minicart/ui/navigation/NavGraph.kt
@@ -0,0 +1,38 @@
+package com.kalcreations.minicart.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.minicart.ui.screens.productlist.ProductListScreen
+import com.minicart.ui.screens.cart.CartScreen
+import com.minicart.ui.screens.checkout.CheckoutSuccessScreen
+
+@Composable
+fun NavGraph(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Routes.PRODUCT_LIST
+    ) {
+        composable(Routes.PRODUCT_LIST) {
+            ProductListScreen(
+                onCartClick = { navController.navigate(Routes.CART) }
+            )
+        }
+
+        composable(Routes.CART) {
+            CartScreen(
+                onCheckoutSuccess = { navController.navigate(Routes.CHECKOUT) }
+            )
+        }
+
+        composable(Routes.CHECKOUT) {
+            CheckoutSuccessScreen(
+                onBackToHome = { navController.navigate(Routes.PRODUCT_LIST) }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/kalcreations/minicart/ui/navigation/Routes.kt
@@ -0,0 +1,7 @@
+package com.kalcreations.minicart.ui.navigation
+
+object Routes {
+    const val PRODUCT_LIST = "product_list"
+    const val CART = "cart"
+    const val CHECKOUT = "checkout"
+}
